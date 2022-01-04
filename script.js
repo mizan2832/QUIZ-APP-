d9efb181b72dcccdf1fd9851b56282ef95d6fdb4
mizan2832 <=>
2022-01-04 23:55:01 +0600

    script page

---
+++ script.js
@@ -0,0 +1,63 @@
+const quizData = [
+    {
+        question : "What is the most used programming language in 2022?",
+        a: 'Java',
+        b: 'C',
+        C: 'Javascript',
+        D: 'Python',
+        correct : "d",
+    },
+    {
+        question : "Who is the prime minister of Bangladesh",
+        a: "Shekh Hasina",
+        b: "Khaleda zia",
+        c: "Zillur Rahman",
+        d: 'Arzad hossain',
+        correct : 'a',
+    },
+    
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Jason Object Notation",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+
+];
+const quiz = document.getElementById("quiz");
+const answerEl = document.querySelectorAll('.answer');
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = dcoument.getElementById("b_text");
+const c_text = dcoument.getElementById("c_text");
+const d_text = dcoument.getElementById("d_text");
+
+const submitBtn = dcoument.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
