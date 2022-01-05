dbe710246f3689733f02beb4d31c68adac000d48
mizan2832 <=>
2022-01-05 23:05:00 +0600

    complete quiz app

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ const quiz = document.getElementById("quiz");
 const answerEl = document.querySelectorAll('.answer');
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
-const b_text = dcoument.getElementById("b_text");
-const c_text = dcoument.getElementById("c_text");
-const d_text = dcoument.getElementById("d_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
 
-const submitBtn = dcoument.getElementById("submit");
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
 let score = 0;
@@ -60,4 +60,46 @@ function loadQuiz(){
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
+    
+   
 }
+
+function getSelected(){
+    let answer = undefined;
+
+    answerEl.forEach((answerEl)=>{
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+
+function deselectAnswers() {
+    answerEl.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+submitBtn.addEventListener("click",() => {
+
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+            console.log(score)
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            console.log(currentQuiz)
+
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answerd correctly at ${score}/${quizData.length} questions.</h2>
+                <button onclick="location.reload()">Reload</button>
+                `;
+        }
+    }
+})
